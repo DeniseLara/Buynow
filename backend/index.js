e585a83373faeabf5f6c73cac5f74cd8c99a73ff
Denise Lara <denise2001.lara@gmail.com>
2025-07-07 16:49:17 -0400

    Cambios

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,11 @@ app.use(cors({
 
 app.use(express.json());
 
-// Middleware para evitar cache (desarrollo y producción seguro)
+// Middleware para evitar cache 
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-  //res.set('Pragma', 'no-cache');
   res.set('Expires', '0');
-  res.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload'); // Seguridad en las conexiones HTTPS
+  res.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload'); 
   next();
 });
 
@@ -27,19 +26,19 @@ app.get('/', (req, res) => {
   res.send('Servidor backend funcionando correctamente');
 });
 
-// Tu ruta para crear el Payment Intent
+// Ruta para crear el payment intent
 app.post('/create-payment-intent', async (req, res) => {
   console.log('Solicitud recibida en /create-payment-intent');
 
   try {
     const { amount, userId, shippingCost } = req.body;  // Ahora esperamos también el userId
     
-     // Verifica si el amount y userId son válidos
+     // Verificar si el amount es válido
      if (!amount || isNaN(amount) || amount <= 0) {
       return res.status(400).json({ error: 'Amount debe ser mayor que 0' });
     }
 
-     // Verifica que el userId sea válido (opcional, dependiendo de tu lógica de negocio)
+    // Verificar que el userId sea válido 
      if (!userId) {
       return res.status(400).json({ error: 'userId es necesario' });
     }
@@ -47,21 +46,19 @@ app.post('/create-payment-intent', async (req, res) => {
     // Lógica para calcular el costo de envío según el monto
     const expectedShipping = amount >= 5000 ? 0 : 599; // Si el monto >= 50 USD => envío gratis (en centavos)
      
-     // Verifica si el costo de envío enviado es el esperado
-     if (shippingCost !== expectedShipping) {
+    if (shippingCost !== expectedShipping) {
       return res.status(400).json({ error: `El costo de envío debería ser ${expectedShipping / 100} USD` });
     }
 
-    // Total en centavos: productos + envío
-    const amountInCents = amount + shippingCost  // Esto convierte 55.99 dólares a 5599 centavos
+    const amountInCents = amount + shippingCost  
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amountInCents,
       currency: 'usd',
       payment_method_types: ['card'],
       metadata: { 
-        userId,
-      shippingCost: shippingCost.toString() },  // Asociamos el userId al PaymentIntent
+      userId,
+      shippingCost: shippingCost.toString() },  
     });
 
     res.send({
